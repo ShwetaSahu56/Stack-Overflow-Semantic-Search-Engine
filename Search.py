b85db96383bab495b58cd997d780947f17448716
ShwetaSahu56 <124025964+ShwetaSahu56@users.noreply.github.com>
2023-02-10 22:29:06 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -20,18 +20,6 @@ def make_clickable(ques_id, title):
     link = "https://stackoverflow.com/questions/"+str(ques_id)
     return f'<a target="_blank" href="{link}">{text}</a>'
 
-st.cache_resource
-def get_model_files():
-    #LSTM model based word-vector dictionary and Question Embeddings
-    word_dict = pickle.load(open("Best_LSTM_Model_Vocab_Vector_dict.pkl", "rb"))
-    lstm_embedded_questions = pd.read_pickle("Questions_lstm_embeddings_dataset.pkl")
-    tfidf_wtd_ques_vec_embeddings = np.array(lstm_embedded_questions['tfidf_wtd_lstm_embed_questions'].tolist())
-    #get tf-idf vectorizer model
-    questions_tfidf = pickle.load(open("tfidf_model_ques.pickle", "rb"))
-    # we are converting a dictionary with word as a key, and the idf as a value
-    idf_dict = dict(zip(questions_tfidf.get_feature_names_out(), list(questions_tfidf.idf_)))
-    tfidf_vocab = set(questions_tfidf.get_feature_names_out())
-    return word_dict, lstm_embedded_questions, tfidf_wtd_ques_vec_embeddings, idf_dict, tfidf_vocab
 
 def GetSimilarQuestions(query, no_sim_ques):
     ''' This function finds k-most similar questions to the searched query based on their cosine similarity values'''
@@ -42,18 +30,18 @@ def GetSimilarQuestions(query, no_sim_ques):
     query=re.sub(r'[^A-Za-z0-9#+]+',' ',query)
     
     #LSTM model based word-vector dictionary and Question Embeddings
-    #word_dict = pickle.load(open("Best_LSTM_Model_Vocab_Vector_dict.pkl", "rb"))
-    #lstm_embedded_questions = pd.read_pickle("Questions_lstm_embeddings_dataset.pkl")
-    #tfidf_wtd_ques_vec_embeddings = np.array(lstm_embedded_questions['tfidf_wtd_lstm_embed_questions'].tolist())
+    word_dict = pickle.load(open("Best_LSTM_Model_Vocab_Vector_dict.pkl", "rb"))
+    lstm_embedded_questions = pd.read_pickle("Questions_lstm_embeddings_dataset.pkl")
+    tfidf_wtd_ques_vec_embeddings = np.array(lstm_embedded_questions['tfidf_wtd_lstm_embed_questions'].tolist())
     
     #get tf-idf vectorizer model
-    #questions_tfidf = pickle.load(open("tfidf_model_ques.pickle", "rb"))
+    questions_tfidf = pickle.load(open("tfidf_model_ques.pickle", "rb"))
 
     # we are converting a dictionary with word as a key, and the idf as a value
-    #idf_dict = dict(zip(questions_tfidf.get_feature_names_out(), list(questions_tfidf.idf_)))
-    #tfidf_vocab = set(questions_tfidf.get_feature_names_out())
+    idf_dict = dict(zip(questions_tfidf.get_feature_names_out(), list(questions_tfidf.idf_)))
+    tfidf_vocab = set(questions_tfidf.get_feature_names_out())
     
-    word_dict, lstm_embedded_questions, tfidf_wtd_ques_vec_embeddings, idf_dict, tfidf_vocab = get_model_files()
+    #word_dict, lstm_embedded_questions, tfidf_wtd_ques_vec_embeddings, idf_dict, tfidf_vocab = get_model_files()
     
     #convert query words to vectors if word is present in both model vocabulary and tf-idf vectorizer vocabulary 
     query_vector = np.array([word_dict[w] for w in query.split() if w in word_dict and w in tfidf_vocab])
