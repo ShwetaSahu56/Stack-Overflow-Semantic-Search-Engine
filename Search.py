0735baeb29f70f12f01abd5e65a918bde9eaaf79
ShwetaSahu56 <124025964+ShwetaSahu56@users.noreply.github.com>
2023-02-10 21:41:33 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -20,7 +20,7 @@ def make_clickable(ques_id, title):
     link = "https://stackoverflow.com/questions/"+str(ques_id)
     return f'<a target="_blank" href="{link}">{text}</a>'
 
-@st.cache
+st.cache_data
 def get_model_files():
     #LSTM model based word-vector dictionary and Question Embeddings
     word_dict = pickle.load(open("Best_LSTM_Model_Vocab_Vector_dict.pkl", "rb"))
